33fdc933fc88e8d94a9a8f5c432c3ad4a77d8de7
charbel-mehou <charbel.mehou@groupe-esigelec.org>
2023-08-26 13:47:16 +0200

    ✨ Adding some packages

--- fitness_app/src/App.js
+++ fitness_app/src/App.js
@@ -6,14 +6,17 @@ import ExerciseDetail from './pages/ExerciseDetail'
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import Tips from './components/Tips'
+import './translation/i18n'
 
 const App=() =>{
     return (
-        <Box fullwidth>
+        <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
             <Navbar/>
                 <Routes>
                     <Route path="/" element={<Home/>}/>
-                    <Route path="/exercise/:id" element={<ExerciseDetail/>}/>
+                    <Route path="/exercise/:id" element={<ExerciseDetail/>}/>                  
+                    <Route path="/tips" element={<Tips/>}/>
                 </Routes>
             <Footer/>
          </Box>
